bed0401c2d68bd7e8f10152af4a7e10c1e743ab7
zahra boojar <zahraboojar866@gmail.com>
2024-08-12 10:53:58 +0330

    add empty file and add html file

---
+++ vendor/composer/installed.php
@@ -0,0 +1,38 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c0829e5dc230585ffc0ae82a08a5ba39c33c06f6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c0829e5dc230585ffc0ae82a08a5ba39c33c06f6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'flightphp/core' => array(
+            'pretty_version' => 'v3.11.1',
+            'version' => '3.11.1.0',
+            'reference' => '7cfaca8f1686b3edd7cb1592ec928ad695e38be3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../flightphp/core',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'mikecao/flight' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.0.2',
+            ),
+        ),
+    ),
+);
